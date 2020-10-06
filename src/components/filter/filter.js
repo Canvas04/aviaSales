19f715aab5c263404fba2999ee2f0feaf441cff1
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-06 13:56:42 +0300

    Сделал панель таба

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,17 +1,24 @@
+import Search from "antd/lib/input/Search";
 import React from "react";
+import TabPanel from "../tab-panel";
 import Checkbox from "./checkbox/";
 import classes from "./filter.module.scss";
-const { filter, header } = classes;
+const { filter, header, aside ,search} = classes;
 
 export default function Filter() {
   return (
     <div className={filter}>
-      <h1 className={header}>Количество пересадок</h1>
-      <Checkbox label={'Все'} />
-      <Checkbox label={'Без пересадок'} />
-      <Checkbox label={'1 пересадка'} />
-      <Checkbox label={'2 пересадки'} />
-      <Checkbox label={'3 пересадки'} />
+      <aside className={aside}>
+        <h1 className={header}>Количество пересадок</h1>
+        <Checkbox label={"Все"} />
+        <Checkbox label={"Без пересадок"} />
+        <Checkbox label={"1 пересадка"} />
+        <Checkbox label={"2 пересадки"} />
+        <Checkbox label={"3 пересадки"} />
+      </aside>
+      <aside className={search}>
+          <TabPanel />
+      </aside>
     </div>
   );
 }
