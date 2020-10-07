80d7ca590ca96b4188504036f3b494cae5feafae
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-07 12:24:06 +0300

    Сделал медиа-запросы

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -3,15 +3,15 @@ import TabPanel from "../tab-panel";
 import Checkbox from "./checkbox/";
 import classes from "./filter.module.scss";
 import {useMediaQuery } from 'react-responsive';
-const { filter, header, aside ,search} = classes;
+const { filter, header, aside ,search,filterSm,asideSm,searchSm } = classes;
 
 export default function Filter() {
   const mobileScreen = useMediaQuery({ query: "(max-width: 567px)" });
-
   const laptopScreen = useMediaQuery({ query: "(min-width: 567px)" });
   return (
     <>
-    <FilterLg />
+   {mobileScreen && <FilterSm />} 
+   {laptopScreen && <FilterLg />}
     </>
   );
 }
@@ -34,6 +34,18 @@ function FilterLg() {
 }
 function FilterSm () {
   return <>
-  
+ <div className={filterSm}>
+      <aside className={asideSm}>
+        <h1 className={header}>КОЛИЧЕСТВО ПЕРЕСАДОК</h1>
+        <Checkbox label={"Все"} />
+        <Checkbox label={"Без пересадок"} />
+        <Checkbox label={"1 пересадка"} />
+        <Checkbox label={"2 пересадки"} />
+        <Checkbox label={"3 пересадки"} />
+      </aside>
+      <aside className={searchSm}>
+          <TabPanel />
+      </aside>
+    </div>
   </>
 }
