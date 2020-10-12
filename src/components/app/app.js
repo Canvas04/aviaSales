81a876f7edd183130bb60677286dd007120582c8
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-12 13:02:39 +0300

    Оптимизировал index.js

---
+++ src/components/app/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "normalize.css";
+import classes from './index.module.scss';
+import Logo from '../logo';
+import Filter from '../filter';
+import {useMediaQuery} from 'react-responsive';
+
+const { mainLg ,mainSm} = classes;
+
+function MainLg() {
+    return (
+      <main className={mainLg}>
+        <Logo />
+        <Filter />
+      </main>
+    );
+  }
+  function MainSM() {
+    return (
+      <main className={mainSm}>
+        <Logo />
+        <Filter />
+      </main>
+    );
+  }
+  const App = () => {
+    const mobileScreen = useMediaQuery({ query: "(max-width: 567px)" });
+    const laptopScreen = useMediaQuery({ query: "(min-width: 567px)" });
+    return <>
+    {laptopScreen && <MainLg />}
+    {mobileScreen && <MainSM />}
+    </>;
+  };
+
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 import React from "react";
-import "normalize.css";
 import ReactDom from "react-dom";
-import classes from "./index.module.scss";
-import Logo from "./components/logo";
-import Filter from "./components/filter/filter";
-import { useMediaQuery } from "react-responsive";
+import App from "./components/app";
 
-const { mainLg ,mainSm} = classes;
 
-const App = () => {
-  const mobileScreen = useMediaQuery({ query: "(max-width: 567px)" });
-  const laptopScreen = useMediaQuery({ query: "(min-width: 567px)" });
-  return <>
-  {laptopScreen && <MainLg />}
-  {mobileScreen && <MainSM />}
-  </>;
-};
 ReactDom.render(<App />, document.querySelector("#root"));
 
-function MainLg() {
-  return (
-    <main className={mainLg}>
-      <Logo />
-      <Filter />
-    </main>
-  );
-}
-function MainSM() {
-  return (
-    <main className={mainSm}>
-      <Logo />
-      <Filter />
-    </main>
-  );
-}
