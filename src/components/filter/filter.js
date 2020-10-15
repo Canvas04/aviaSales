b2f6ee3e504cb917fb9b30df7c0557d5eaf55df5
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-15 16:07:33 +0300

    Отобразил из стора чекбоксы

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -2,50 +2,66 @@ import React from "react";
 import TabPanel from "../tab-panel";
 import Checkbox from "./checkbox/";
 import classes from "./filter.module.scss";
-import {useMediaQuery } from 'react-responsive';
-const { filter, header, aside ,search,filterSm,asideSm,searchSm } = classes;
+import { useMediaQuery } from "react-responsive";
+import { connect } from "react-redux";
 
-export default function Filter() {
+const { filter, header, aside, search, filterSm, asideSm, searchSm } = classes;
+const mapStateToProps = (state) => {
+  return {
+    checkboxes: state.checkboxes,
+  };
+};
+const FinishedComponentLG = connect(mapStateToProps)(FilterLg);
+const FinishedComponentSm = connect(mapStateToProps)(FilterSm);
+
+function Filter() {
   const mobileScreen = useMediaQuery({ query: "(max-width: 567px)" });
   const laptopScreen = useMediaQuery({ query: "(min-width: 567px)" });
   return (
     <>
-   {mobileScreen && <FilterSm />} 
-   {laptopScreen && <FilterLg />}
+      {mobileScreen && <FinishedComponentSm/>}
+      {laptopScreen && <FinishedComponentLG />}
     </>
   );
 }
-function FilterLg() {
+function FilterLg({ checkboxes }) {
+  const {all,noStops,oneStop,twoStops,threeStops} = checkboxes;
   return (
     <div className={filter}>
       <aside className={aside}>
         <h1 className={header}>КОЛИЧЕСТВО ПЕРЕСАДОК</h1>
-        <Checkbox label={"Все"} />
-        <Checkbox label={"Без пересадок"} />
-        <Checkbox label={"1 пересадка"} />
-        <Checkbox label={"2 пересадки"} />
-        <Checkbox label={"3 пересадки"} />
+        <Checkbox label={all.text} />
+        <Checkbox label={noStops.text} />
+        <Checkbox label={oneStop.text} />
+        <Checkbox label={twoStops.text} />
+        <Checkbox label={threeStops.text} />
       </aside>
       <aside className={search}>
-          <TabPanel />
+        <TabPanel />
       </aside>
     </div>
   );
 }
-function FilterSm () {
-  return <>
- <div className={filterSm}>
-      <aside className={asideSm}>
-        <h1 className={header}>КОЛИЧЕСТВО ПЕРЕСАДОК</h1>
-        <Checkbox label={"Все"} />
-        <Checkbox label={"Без пересадок"} />
-        <Checkbox label={"1 пересадка"} />
-        <Checkbox label={"2 пересадки"} />
-        <Checkbox label={"3 пересадки"} />
-      </aside>
-      <aside className={searchSm}>
+function FilterSm({checkboxes}) {
+    const {all,noStops,oneStop,twoStops,threeStops} = checkboxes;
+  return (
+    <>
+      <div className={filterSm}>
+        <aside className={asideSm}>
+          <h1 className={header}>КОЛИЧЕСТВО ПЕРЕСАДОК</h1>
+          <Checkbox label={all.text} />
+        <Checkbox label={noStops.text} />
+        <Checkbox label={oneStop.text} />
+        <Checkbox label={twoStops.text} />
+        <Checkbox label={threeStops.text} />
+        </aside>
+        <aside className={searchSm}>
           <TabPanel />
-      </aside>
-    </div>
-  </>
+        </aside>
+      </div>
+    </>
+  );
 }
+
+
+export default connect(mapStateToProps)(Filter);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,5 +2,4 @@ import {createStore } from 'redux';
 import {rootReducer,initialState} from '../reducers';
 
 const store =createStore(rootReducer,initialState)
-
 export {store};
