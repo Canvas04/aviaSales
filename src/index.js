7508bb0b73a5eab35f719b47bc044a0e16558e04
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-21 13:45:01 +0300

    Добавил redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/app";
 import { Provider } from "react-redux";
-import {createStore,applyMiddleware,compose} from 'redux';
-import rootReducer from './reducers';
-function loggerMiddleWare(store) {
-  return function (next ) {
-    return function (action) {
-      const result  = next(action);
-      console.log('MiddleWare',store.getState());
-      return result;
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from 'redux-thunk';
+import rootReducer from "./reducers";
 
-    }
-  }
-}
+const loggerMiddleWare = (store) => (next) => (action) => {
+  const result = next(action);
+  console.log("MiddleWare", store.getState());
+  return result;
+};
+
 const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ }) : compose;
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
-
-const store = createStore(rootReducer,composeEnhancers(applyMiddleware(loggerMiddleWare)))
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(loggerMiddleWare))
+);
 ReactDom.render(
   <Provider store={store}>
     <App />
