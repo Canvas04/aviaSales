91a5d843152995506349bacd1280f1ddc15627f1
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-26 11:46:25 +0300

    Рефакторинг

--- src/secondaryFunc/index.js
+++ src/secondaryFunc/index.js
@@ -1,26 +1,10 @@
 
-// const cheapTickets = exampleObj.sort((a, b) => {
-//   return a.price - b.price;
-// });
-// const viewCheapTickets = cheapTickets.slice(0, 20);
-
-// //Самый быстрый
-// const speedTickets = exampleObj.sort((a, b) => {
-//   return (
-//     a.segments.reduce((acc, current) => acc.duration + current.duration) -
-//     b.segments.reduce((acc, current) => acc.duration + current.duration)
-//   );
-// });
-// const viewSpeedTickets = speedTickets.slice(0, 20);
-//Функция фильтр в зависимости от цены
 export const filterOnPrice = (arr) => {
   const copyArr = arr.slice();
   return copyArr.sort((a, b) => {
     return a.price - b.price;
   });
 };
-//Функция фильтр в зависимости от скорости
-
 export const filterOnSpeed = (arr) => {
   const copyArr = arr.slice();
   return copyArr.sort((a, b) => {
@@ -30,7 +14,7 @@ export const filterOnSpeed = (arr) => {
     );
   });
 }
-// Функция фильтрует длину массива
+
 export const filterOnLength = (arr) => {
   if(arr.length > 20) {
     return arr.slice(0,20)
@@ -38,7 +22,6 @@ export const filterOnLength = (arr) => {
   return arr;
 }
 
-// Функция фильтр в зависимости от количества пересадок
 export const filterOnStops = (arr, num) => {
   const noStops = arr.filter((el) => {
     const { segments } = el;
